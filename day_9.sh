af53952ed3f5681bf2eb168d5ead897666a67681
IndiraShahi <IndiraShahi144@gmail.com>
2021-05-08 01:50:29 +0530

    Function to find daily work hours

--- day_9.sh
+++ day_9.sh
@@ -2,26 +2,36 @@
 
 echo "Welcome to Employee Wage computation Program on Master branch"
 
-Wage_Per_Hour=20
-Max_Work_Hour=100
-Part_Time_Employee=1
-Full_Time_Employee=2
-No_Of_Working_Days=20
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_PART_TIME=2
+EMP_RATE_PER_HR=20
+NO_OF_WORKING_DAYS=20
+MAX_WORK_HOURS=80
+
+
 totalWorkingDays=0
 totalWorkingHours=0
-while [ $totalWorkingDays -lt $No_Of_Working_Days ] && [ $totalWorkingHours -lt $Max_Work_Hour ]
+
+function getWorkHrs(){
+                local empCheck=$1
+                local empHrs=0
+                case $empCheck in
+                $IS_PRESENT_FULL_TIME ) empHrs=8 ;;
+                $IS_PRESENT_PART_TIME ) empHrs=4 ;;
+                *) empHrs=0     ;;
+         esac
+         echo $empHrs
+}
+count=0
+while [ $totalWorkingDays -lt $NO_OF_WORKING_DAYS ] && [ $totalWorkingHours -lt $MAX_WORK_HOURS ]
+
 do
-empCheck=$(( RANDOM%3 ))
-totalWorkingDays=$(( $totalWorkingDays + 1 ))
-case $empCheck in
-        $Part_Time_Employee)
-                empHrs=8   ;;
-        $Full_Time_Employee)
-                empHrs=4   ;;
-        *)
-                empHrs=0   ;;
-esac
-totalWorkingHours=$(( $totalWorkingHours + $empHrs ))
+        empCheck=$(( RANDOM%3 ))
+        totalWorkingDays=$(( $totalWorkingDays + 1 ))
+        empHrs="$( getWorkHrs $empCheck )"
+        totalWorkingHours=$(( $totalWorkingHours + $empHrs ))
 done
-salary=$(( $totalWorkingHours * $Wage_Per_Hour ))
-echo $salary
+echo "$totalWorkingHours: is total work hours"
+
+
+
