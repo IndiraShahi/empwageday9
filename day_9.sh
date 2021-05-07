311bd4677261919ed9d91ca7d43807c173ac27e9
IndiraShahi <IndiraShahi144@gmail.com>
2021-05-07 21:58:15 +0530

    employee daily wage added

--- day_9.sh
+++ day_9.sh
@@ -2,7 +2,8 @@
 
 echo "Welcome to Employee Wage computation Program on Master branch"
 
-
+Wage_Per_Hour=20
+Full_Day_Hour=8
 a=$(( RANDOM%2 ))
 echo $a
 if [ $a -eq 1 ]
@@ -11,3 +12,5 @@ then
 else
       echo "employee is absent"
 fi
+salary=$(( $Wage_Per_Hour * $Full_Day_Hour ))
+echo $salary
