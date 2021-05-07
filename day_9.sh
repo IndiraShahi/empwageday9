2ac160b89c9e88d74b63d1369ecda3a3d7cd3cd1
IndiraShahi <IndiraShahi144@gmail.com>
2021-05-08 01:12:44 +0530

    wage for whole month

--- day_9.sh
+++ day_9.sh
@@ -3,13 +3,13 @@
 echo "Welcome to Employee Wage computation Program on Master branch"
 
 Wage_Per_Hour=20
-Max_Work_Hour=40
+Max_Work_Hour=160
 Part_Time_Employee=1
 Full_Time_Employee=2
-No_Of_Working_Days=5
+No_Of_Working_Days=20
 totalWorkingDays=0
 totalWorkingHours=0
-while [ $totalWorkingDays -lt $No_Of_Working_Days ] && [ $totalWorkingHours -lt $Max_Work_Hour ]
+while [ $totalWorkingHours -lt $Max_Work_Hour ]
 do
 empCheck=$(( RANDOM%3 ))
 totalWorkingDays=$(( $totalWorkingDays + 1 ))
@@ -23,5 +23,5 @@ case $empCheck in
 esac
 totalWorkingHours=$(( $totalWorkingHours + $empHrs ))
 done
-salary=$(( $empHrs * $Wage_Per_Hour ))
+salary=$(( $totalWorkingHours * $Wage_Per_Hour ))
 echo $salary
